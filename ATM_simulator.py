8ae5b2df7b2bc2bbd7cb3c84476606b4bbc9c515
JamaldeenS4 <jamaldeensalawu08@gmail.com>
2025-11-06 16:41:43 +0100

    Improve Dashboard for cleaner interface

--- ATM_simulator.py
+++ ATM_simulator.py
@@ -50,9 +50,8 @@ while True:
     print ("\n1.Check Balance")
     print ("2.Deposit")
     print ("3.Withdraw")
-    print ("4.Change pin")
-    print ("5.Transfer")
-    print ("6.Exit")
+    print ("4.More")
+    print ("5.Exit")
 
     choice = input("Your choice: ")
     
@@ -73,34 +72,45 @@ while True:
             print ("You successfully withdrawed: $", withdraw)
             balance -= withdraw
             print ("Your balance: $", balance)
-
-    elif choice == "4" or choice == "Change pin":
-        current_pin = int(input("Enter Current PIN: "))
-        if current_pin == correct_pin:
-            new_pin = int(input("Enter New PIN: "))
-            confirm_pin = int(input("Confirm New PIN: "))
-            if new_pin == confirm_pin:
-                print("PIN successfully Changed")
-            else:
-                print("PIN confirmation does not match.")
-        else:
-            print("Incorrect PIN")
     
-    elif choice == "5" or choice == "Transfer":
-        target_acct = input("Enter Target Account: ")
-        if len(target_acct) == 10:
-            target_acct = input("Enter Bank Account")
-            amount = int(input("Enter Amount: "))
-            if amount >  balance:
-                print("Insufficient Balance")
+    elif choice == "4" or choice == "More":
+        print("\n6.Transfer")
+        print("7.Change pin")
+        choice = input("Enter your choice: ")
+        if choice == "6" or choice == "Transfer":
+            target_acct = input("Enter Target Account: ")
+            if len(target_acct) == 10:
+                print("\nAvailable Banks:")
+                print("1.GT Bank")
+                print("2.Momo PSB")
+                print("3.Access Bank")
+                print("4.First Bank")
+                target_bank = input("Enter Bank Account: ")
+                amount = int(input("Enter Amount: "))
+                if amount >  balance:
+                    print("Insufficient Balance")
+                else:
+                    balance -= amount
+                    print("Transfer of ${} to {} was Successful".format(amount, target_acct))
+                    print("Balance: ${}".format(balance))
             else:
-                balance -= amount
-                print("Transfer of ${} was Successful".format(amount))
-                print("Balance: ${}".format(balance))
+                print("Invalid Account Number")
+        elif choice == "7" or choice == "Change pin":
+            current_pin = int(input("Enter Current PIN: "))
+            if current_pin == correct_pin:
+                new_pin = int(input("Enter New PIN: "))
+                confirm_pin = int(input("Confirm New PIN: "))
+                if new_pin == confirm_pin:
+                    print("PIN successfully Changed")
+                else:
+                    print("PIN confirmation does not match.")
+            else:
+                print("Incorrect PIN")
+        
         else:
-            print("Invalid Account Number")
-
-    elif choice == "6" or choice == "Exit":
+            print("Invalid Choice")
+  
+    elif choice == "5" or choice == "Exit":
         print("See you soon")
         break
 
